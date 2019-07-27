9204f82c4bb6120fcb0dc3fa545be5b38d823705
Jacy Gao <aus.jacy@gmail.com>
2019-07-27 23:53:00 +1000

    removed notebooks endpoints

--- src/config.js
+++ src/config.js
@@ -7,10 +7,6 @@ export const AUTH_USER = AUTH_API + "/user/login"
 export const POST_USER = AUTH_API + "/user/signup"
 export const VERIFY_USER = AUTH_API + "/user/verify"
 
-/* Notebook */
-export const GET_NOTEBOOKS = RESOURCE_API + "/notebooks"
-export const POST_NOTEBOOK = RESOURCE_API + "/notebook"
-
 /* Note */
 export const GET_NOTES_API = RESOURCE_API + "/notes"
 export const POST_NOTE_API = RESOURCE_API + "/note"
