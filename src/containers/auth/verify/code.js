13e8ba40abc27f61b576371accfe0fb6febe5a34
Jacy Gao <aus.jacy@gmail.com>
2019-07-29 23:25:07 +1000

    changed verification code input to string type

--- src/containers/auth/verify/code.js
+++ src/containers/auth/verify/code.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const CodeContainer = ({onKeyDown, onChange, onNext, error}) => (
   <div className="container-email-and-button clearfix">
-    <input className={"_input"+(error === "" ? "" : " incorrect-input")} type="number" placeholder="Enter activation code" title="Enter the activation code in the email we have sent you"
+    <input className={"_input"+(error === "" ? "" : " incorrect-input")} type="text" placeholder="Enter activation code" title="Enter the activation code in the email we have sent you"
     onKeyDown={onKeyDown} 
     onChange={onChange} />
     <p className={"text-error" + (error === "" ? "" : " text-error-visible" )}>{error}</p>
