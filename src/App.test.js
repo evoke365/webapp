a4ddafb98d2b6652bc6a53b3611b6f2d6f1199a8
Jacy Gao <aus.jacy@gmail.com>
2020-07-29 22:11:22 +1000

    fixed unit test to pass build

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './App';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AppRouter />, div);
+  ReactDOM.render(    
+  <Provider store = {createStore(reducers)}>
+    <AppRouter />
+  </Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
