adcc48e29614b37755742b770539781ff71cd140
Jacy Gao <aus.jacy@gmail.com>
2020-08-22 19:38:36 +1000

    - renamed app name to evoke365 - implemented a shared component for logo container - added a basic test for auth container - minor ui updates

--- src/containers/auth/login/index.js
+++ src/containers/auth/login/index.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { submitPassword, forgetPassword, clearError } from '../../../actions/login'
 import { logout } from '../../../actions/logout'
 import { loadState, saveState } from '../../../localStorage'
+import LogoContainer from '../logo'
 import PasswordContainer from '../password'
 import Slide from '@material-ui/core/Slide';
 import { withStyles } from '@material-ui/core/styles'
@@ -106,9 +107,7 @@ class LoginContainer extends Component{
       <div className={classes.container}>
         <Slide direction="down" in={true} mountOnEnter unmountOnExit>
           <div>
-            <div className={classes.header}>
-              <p className={classes.logo}>studybox.io</p>
-            </div>
+            <LogoContainer />
             <div className={classes.message}>Welcome back, {email}</div>
             
           </div>
@@ -130,21 +129,10 @@ const style = theme => ({
     maxWidth: '1024px',
     paddingTop: '10%',
   },
-  header: {
-    textAlign: 'center',
-  },
   message: {
     fontSize: '1.2em',
     color: '#000000',
     textAlign: 'center',
-  },
-  logo: {
-    minHeight: '55px',
-    fontSize: '2em',
-    fontWeight: '300',
-    fontStyle: 'italic',
-    color: 'rgb(251, 167, 59)',
-    marginLeft: '10px',
   },
 })
 
